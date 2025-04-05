66092fd95461ba0607974fa27545c75c7806e2e9
Shah Wali <69451223+shahwalidjango@users.noreply.github.com>
2025-04-05 10:07:26 +0500

    Currenty Converter

--- tesing.py
+++
@@ -1,9 +0,0 @@
-class Solution(object):
-    def maxArea(self, height):
-        length = len(height)
-        ls = []
-        for r in range(length):
-            for l in range(length):
-                ls.append((length-r)* min(height[l], height[r]))
-        return max(ls)
-       
